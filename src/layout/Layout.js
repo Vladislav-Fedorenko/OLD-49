56db7cc585fbeef4b78bb0412197808afc1bc7a4
Vladislav Fedorenko <fedorenko.vladislav.k@gmail.com>
2021-10-02 18:06:18 +0600

    Working with audio has been initialized

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
+import ReactAudioPlayer from 'react-audio-player';
 
+import {AppContext} from './context/Context';
 import {Progressbar} from '../components/progressbar/Progressbar';
 import {Start} from '../pages/start/Start';
-
-import './style.css';
 import {Bar} from '../pages/bar/Bar';
 
-export const Layout = (props) => {
+import './style.css';
+
+export const Layout = () => {
+  const {audioTrack} = useContext(AppContext);
   return (
     <div className="layout">
+      <ReactAudioPlayer src={audioTrack} autoPlay={true} controls={false} />
       <div className="layout__progress">
         <Progressbar />
       </div>
@@ -29,4 +33,4 @@ export const Layout = (props) => {
       </Router>
     </div>
   );
-}
+};
--- src/pages/start/Start.js
+++ src/pages/start/Start.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-import "./style.scss";
-import { AlkoImage } from "../../components/alkoimage/AlkoImage";
-import { Link } from "react-router-dom";
+import {AppContext} from '../../layout/context/Context';
+import {AlkoImage} from '../../components/AlkoImage/AlkoImage';
+
+import audio from '../../assets/psychomecka.ogg';
+import './style.scss';
 
 export const Start = () => {
+  const {setAudioTrack} = useContext(AppContext);
+  useEffect(() => setAudioTrack(audio), []);
   return (
     <div className="start-page">
       <AlkoImage>
