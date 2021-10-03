3b103332f4bcdfd186ea999f8d8ebf3c9c3f1798
Roman Gorev <ogogorev@yandex.ru>
2021-10-03 14:21:59 +0600

    Update AlkoImage

--- src/components/alkoimage/AlkoImage.js
+++ src/components/alkoimage/AlkoImage.js
@@ -1,31 +1,68 @@
-import {useContext, useMemo} from "react";
+import { useContext, useMemo, useState } from "react";
 import "./AlkoImage.scss";
-import {AppContext} from '../../layout/context/Context';
+import { AppContext } from "../../layout/context/Context";
+import { r } from "../../utils/random";
+import { useInterval } from "../../hooks/useInterval";
 
-const SHAKE = 10;
+const SHAKE = 25;
 const BLUR = 5;
+const ROTATE = 25;
+
+const normIntensity = (int) => {
+  if (int > 100) {
+    return 1;
+  }
+  return int / 100;
+};
 
 const getCssProps = (intensity) => {
-  const r = Math.floor(-5 + Math.random() * 10);
-  const shake = intensity * SHAKE * r / 20;
-  const blur = intensity * BLUR / 20;
+  const k = 1 + intensity / 100;
+  const shakeX = r({ a: -SHAKE }) * k;
+  const shakeY = r({ a: -SHAKE }) * k;
+
+  const blur = BLUR * k;
 
   const cssProps = {
-    "--shake": `${shake}px`,
+    "--shake-x": `${shakeX}px`,
+    "--shake-y": `${shakeY}px`,
     "--blur": `${blur}px`,
   };
 
   return cssProps;
 };
 
+// const f = (intensity) => {
+//   const n = normIntensity(intensity);
+
+//   console.log("N", n);
+
+//   return {
+//     // opacity: `${1 - n}`,
+//     // opacity: `${n}`,
+//     transform: `rotate(${r({ a: -ROTATE, b: 2 * ROTATE }) * n}deg)`,
+//   };
+// };
+
 export const AlkoImage = ({ intensity = null, children }) => {
-  const {progressbarValue} = useContext(AppContext);
-  const cssProps = useMemo(() => getCssProps(intensity || progressbarValue),
-    [progressbarValue, intensity]);
+  const { progressbarValue } = useContext(AppContext);
+  const [cssProps, setCssProps] = useState({});
+  // const [cssCopyProps, setCssCopyProps] = useState({});
+
+  useInterval(() => {
+    setCssProps(getCssProps(intensity || progressbarValue));
+  }, 300);
+
+  // useInterval(() => {
+  //   setCssCopyProps(f(intensity || progressbarValue));
+  // }, 1000);
 
   return (
     <div className="alko-image" style={cssProps}>
       {children}
+
+      {/* <div className="alko-image-copy" style={cssCopyProps}>
+        {children}
+      </div> */}
     </div>
   );
 };
